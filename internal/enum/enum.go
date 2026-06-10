390102de352e5ead9e5541dc3a9f80a9718357a5
ReinUrmet <rein.urmet@gmail.com>
2026-06-10 14:50:44 +0300

    wip: add concurrent lookups with goroutines and WaitGroup

--- internal/enum/enum.go
+++ internal/enum/enum.go
@@ -13,15 +13,17 @@ func BasicLookup(domain string, wordlist io.Reader) {
 	buf := bufio.NewScanner(wordlist)
 
 	for buf.Scan() {
-		prefix := buf.Text()
-		subdomain := prefix + "." + domain
-		address, err := net.LookupHost(subdomain)
-		if err != nil {
-			continue
-		} else {
-			fmt.Println(subdomain)
-			fmt.Println(address)
-			fmt.Println()
-		}
+		go func() {
+			prefix := buf.Text()
+			subdomain := prefix + "." + domain
+			address, err := net.LookupHost(subdomain)
+			if err != nil {
+				return
+			} else {
+				fmt.Println(subdomain)
+				fmt.Println(address)
+				fmt.Println()
+			}
+		}()
 	}
 }
