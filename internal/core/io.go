e965e74ebc3a698e110335e7d193be55830f0526
reinurmet <rein.urmet@ut.ee>
2026-07-01 22:47:04 +0300

    feat: add TargetReader to read host lists from a stream into []core.Host

--- internal/core/io.go
+++ internal/core/io.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -11,9 +12,27 @@ func PlainWriter(writer io.Writer, host Host) {
 	fmt.Fprintln(writer, host.Name)
 }
 
+//TODO
+//NDJSON writer buggy (prints stray <nil>)
+
 // for front end use later
 func NdjsonWriter(writer io.Writer, host Host) {
 	encoder := json.NewEncoder(writer)
 	jsonResult := encoder.Encode(host)
 	fmt.Fprintln(writer, jsonResult)
+}
+
+//TODO
+//NDJSON parsing + stdin/-f wiring
+
+// For reading lists (reqired for tools like: probe, dns, takeover)
+func TargetReader(input io.Reader) []Host {
+	buf := bufio.NewScanner(input)
+	lines := []Host{}
+
+	for buf.Scan() {
+		line := buf.Text()
+		lines = append(lines, Host{Name: line})
+	}
+	return lines
 }
